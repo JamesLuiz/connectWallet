d3eaa9949db1baf893e4ac904bc0a15ee8fca0b4
James Luiz <jamesluiz116@gmail.com>
2023-08-02 20:11:27 +0100

    connnect wallet

--- public/connectMetamask.js
+++ public/connectMetamask.js
@@ -2,8 +2,10 @@ window.addEventListener('load', async () => {
     if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
         try {
-            await window.ethereum.enable();
-            const accounts = await web3.eth.getAccounts();
+            let accounts = await web3.eth.getAccounts();
+            if (accounts.length == 0) { // check if Metamask is connected
+                accounts = await window.ethereum.request({ method: 'eth_requestAccounts' }); // connect to Metamask
+            }
             console.log('Connected accounts:', accounts);
         } catch(error) {
             console.error("User rejected connection:", error);
